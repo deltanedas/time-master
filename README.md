965128f5d57a716300f6443c3e0c8f473b05134e
DeltaNedas <deltanedas@gmail.com>
2019-12-18 19:37:59 +0000

    doesnt work

---
+++ README.md
@@ -0,0 +1,3 @@
+# Time Master
+This mod requires at least Mindustry build **101** on PC or Android.
+It adds a sandbox-only Clock block that lets you advance or reverse time by an hour.
---
+++ scripts/clock.js
@@ -0,0 +1,30 @@
+const region = Core.atlas.find("time-master-clock");
+
+const clock = extendContent(Block, "clock", {
+	buildConfiguration: function(tile, table){
+		table.addImageButton(Icon.arrowLeftSmall, Styles.clearTransi, run(() => {
+			tile.configure(-1);
+		})).size(35);
+		table.addImageButton(Icon.arrowRightSmall, Styles.clearTransi, run(() => {
+			tile.configure(1);
+		})).size(35);
+	},
+
+	configured: function(tile, player, value){
+		this.hour += value * (1 / 24);
+		this.hour %= 1;
+	},
+
+	update: function(tile){
+		Vars.state.rules.ambientLight.a = Mathf.lerp(Vars.state.rules.ambientLight.a, this.hour, 0.1);
+	},
+
+	draw: function(tile){
+		Draw.rect(region, tile.drawx(), tile.drawy());
+		Lines.stroke(2, Color.scarlet);
+		Lines.lineAngle(tile.drawx(), tile.drawy(), Vars.state.rules.ambientLight.a * 720, 6);
+		Draw.color();
+	}
+});
+
+clock.hour = Vars.state.rules.ambientLight.a;
